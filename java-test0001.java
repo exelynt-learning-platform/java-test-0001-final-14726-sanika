aba4d2be019ce1a74439fe710a3db12ae58c1049
Sanika Barguje <bargujesanika02@gmail.com>
2026-03-14 14:08:17 +0530

    Update java-test0001.java

--- java-test0001.java
+++ java-test0001.java
@@ -3,18 +3,18 @@ public class Pattern {
 
         int n = 5;
 
-        // Upper part
+  
         for (int i = 1; i <= n; i++) {
 
-            // leading spaces
+            
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            // first star
+          
             System.out.print("*");
 
-            // inner spaces
+           
             if (i > 1) {
                 for (int j = 1; j <= 2 * i - 3; j++) {
                     System.out.print(" ");
@@ -25,18 +25,19 @@ public class Pattern {
             System.out.println();
         }
 
-        // Lower part
+        
         for (int i = n - 1; i >= 1; i--) {
 
-            // leading spaces
+            
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            // first star
+          
             System.out.print("*");
 
-            // inner spaces
+           
+            
             if (i > 1) {
                 for (int j = 1; j <= 2 * i - 3; j++) {
                     System.out.print(" ");
